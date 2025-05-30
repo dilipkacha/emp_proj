854e0dfeb20b607672fa1907c911a7d724f13788
dilipkacha <dilipkacha02@gmail.com>
2025-05-30 16:57:35 +0530

    Delete app directory

--- app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import emp
-
-# Register your models here.
-admin.site.register(emp)
--- app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class emp(models.Model):
-    emp_nm=models.CharField(max_length=100)
-    dep=models.CharField(max_length=100)
-    salary=models.IntegerField()
-    
